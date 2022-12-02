2ae22b51573e9b838c8dc2fec39c5175e94b7061
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2022-12-01 22:04:29 -0300

    commit inicial. Excerício 7

---
+++ src/exercicio7/Exercicio7.java
@@ -0,0 +1,39 @@
+/*7) Considere um vetor com 50 números inteiros gerados aleatoriamente de 1 até 100. Faça um algoritmo
+recursivo para imprimir o maior valor deste vetor.
+Crie e utilize uma função para preencher o vetor e uma função recursiva para encontrar o maior valor do
+vetor. Esta informação tem que ser impressa na função main.
+Obs. 1: Não é permitido utilizar qualquer estrutura de dados auxiliar;
+Obs. 2: Não é permitido utilizar qualquer estrutura de repetição na função recursiva. */
+package exercicio7;
+
+import java.util.Random;
+
+public class Exercicio7 {	
+	    
+	    public static void main(String[] args) {
+	        int vetor[] = new int[50];
+	        vetor = filledVector(vetor.length);	        
+	        System.out.println("Maior valor do vetor = " + findLargest(vetor, 0));   
+	    }
+	    
+	    public static int[] filledVector(int size){
+	    	int vetor[] = new int[50];
+	        Random gerador = new Random();
+	        for (int i=0; i<50; i++){
+	            vetor[i]= gerador.nextInt(100) +1;
+	        }
+	        return vetor;
+	    }
+	    
+	    public static int findLargest(int[] vetor, int index) {
+	    	int maior = 0;
+	    	if (index == (vetor.length))
+	    		return 0;
+	    	maior = findLargest(vetor, index+1);
+	    	if (maior > vetor[index])
+	    		return maior;
+	    	else return vetor[index];    	
+	    }
+}
+
+
